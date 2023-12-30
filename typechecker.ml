d5f9579bb4ab00d606218647e8519a99e2f0fd86
tgu <stevengu2019@hotmail.com>
2023-12-30 22:26:24 +0100

    typechecker arithmetrique

--- typechecker.ml
+++ typechecker.ml
@@ -23,59 +23,54 @@ let typecheck_prog p =
     | Int _  -> TInt
     | Bool _ -> TBool
     | Unop (unop, e')  -> 
-      begin
       let type' = type_expr e' tenv in
-      match unop with
-      |Opp -> begin 
-        match type' with 
-        |TInt -> TInt
-        | _ -> failwith "type error"
-      end
-      |Not ->begin 
-        match type' with 
-        |TBool -> TBool
-        | _ -> failwith "type error"
-      end
-    end
-    | Binop(binop, e1, e2) -> 
       begin
-        let type1 = type_expr e1 tenv in
-        let type2 = type_expr e2 tenv in
-      match binop with
-      |Add | Sub | Mul | Div | Rem -> begin
-        match type1,type2 with
-        | TInt, TInt -> TInt
-        | _,_ -> failwith "type error"
+        match unop with
+        | Opp -> if type' = TInt then TInt else type_error type' TInt
+        | Not -> if type' = TBool then TBool else type_error type' TBool
       end
-      |Lt  | Le  | Gt | Ge -> begin
-        match type1,type2 with
-        | TBool,TBool -> TBool
-        | _,_ -> failwith "type error"
+    | Binop(binop, e1, e2) -> 
+      let type1 = type_expr e1 tenv in
+      let type2 = type_expr e2 tenv in
+      begin
+        match binop with
+        | Add | Sub | Mul | Div | Rem ->
+          if type1 = TInt && type2 = TInt then TInt
+          else type_error type1 TInt
+        | Lt  | Le  | Gt | Ge ->
+          if type1 = TBool && type2 = TBool then TBool
+          else type_error type1 TBool
+        | Eq | Neq ->
+          if type1 = type2 then TBool
+          else type_error type1 type2
+        | And | Or ->
+          if type1 = TBool && type2 = TBool then TBool
+          else type_error type1 TBool
       end
-      |Eq | Neq -> begin
-        if type1 = type2 then TBool
-        else failwith "type error"
-      end
-      |And | Or -> begin
-        match type1,type2 with
-        | TBool,TBool -> TBool
-        | _,_ -> failwith "type error"
-      end
-    
-    end
     | Get m -> type_mem_access m tenv
-    | This -> TClass
+    | This -> TClass "current_class"  (* Replace "current_class" with the actual class name *)
     | New c -> TClass c
-    | NewCstr _ -> TInt
-    | MethCall _ -> TInt
+    | NewCstr (class_name, _) -> TClass class_name
+    | MethCall (obj, method_name, _) ->
+      let obj_type = type_expr obj tenv in
+    (* Implement logic to find the return type of the method in obj_type *)
+      match obj_type with
+      | TClass class_name -> TObject class_name  (* Assuming the method returns an instance of the class *)
+      | _ -> failwith "Invalid object type for method call"
     | _ -> failwith "case not implemented in type_expr"
 
   and type_mem_access m tenv = match m with
-    | _ -> failwith "case not implemented in type_mem_access"
+    | Var var_name -> 
+      (try Env.find var_name tenv with Not_found -> failwith ("Variable not found: " ^ var_name))
+    | Field (obj, field_name) ->
+      let obj_type = type_expr obj tenv in
+      (* Implement logic to find the type of the attribute in obj_type *)
+      TBool  (* Replace with the actual type *)
+
   in
 
   let rec check_instr i ret tenv = match i with
-    | Print e -> check e TInt tenv
+    | Print e -> ignore (check e TInt tenv)
     | _ -> failwith "case not implemented in check_instr"
   and check_seq s ret tenv =
     List.iter (fun i -> check_instr i ret tenv) s
