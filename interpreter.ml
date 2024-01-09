2b857de3377979f7463b2f1cc6c7b15cee80bc33
tgu <stevengu2019@hotmail.com>
2024-01-09 23:13:34 +0100

    correct issue in Newconstr

--- interpreter.ml
+++ interpreter.ml
@@ -14,6 +14,83 @@ exception Error of string
 exception Return of value
 let current_object = ref None
 
+(* let rec string_of_expr = function
+  | Int n -> Printf.sprintf "Int(%d)" n
+  | Bool b -> Printf.sprintf "Bool(%b)" b
+  | This -> "This"
+  | Get m -> Printf.sprintf "Get(%s)" (string_of_mem_access m)
+  | Unop (u, e) -> Printf.sprintf "Unop(%s, %s)" (string_of_unop u) (string_of_expr e)
+  | Binop (b, e1, e2) ->
+      Printf.sprintf "Binop(%s, %s, %s)" (string_of_binop b) (string_of_expr e1) (string_of_expr e2)
+  | New cls -> Printf.sprintf "New(%s)" cls
+  | NewCstr (cls, args) ->
+      let args_str = String.concat ", " (List.map string_of_expr args) in
+      Printf.sprintf "NewCstr(%s, [%s])" cls args_str
+  | MethCall (obj, method_name, args) ->
+      let obj_str = string_of_expr obj in
+      let args_str = String.concat ", " (List.map string_of_expr args) in
+      Printf.sprintf "MethCall(%s, %s, [%s])" obj_str method_name args_str
+
+and string_of_unop = function
+  | Opp -> "Opp"
+  | Not -> "Not"
+
+and string_of_binop = function
+  | Add -> "Add"
+  | Sub -> "Sub"
+  | Mul -> "Mul"
+  | Div -> "Div"
+  | Rem -> "Rem"
+  | Lt -> "Lt"
+  | Le -> "Le"
+  | Gt -> "Gt"
+  | Ge -> "Ge"
+  | Eq -> "Eq"
+  | Neq -> "Neq"
+  | And -> "And"
+  | Or -> "Or"
+
+and string_of_mem_access = function
+  | Var v -> Printf.sprintf "Var(%s)" v
+  | Field (e, field) -> Printf.sprintf "Field(%s, %s)" (string_of_expr e) field
+
+
+let rec string_of_value = function
+  | VInt n -> Printf.sprintf "VInt(%d)" n
+  | VBool b -> Printf.sprintf "VBool(%s)" (string_of_bool b)
+  | VObj obj -> Printf.sprintf "VObj(%s)" (string_of_obj obj)
+  | Null -> "Null"
+
+and string_of_obj obj =
+  let buf = Buffer.create 50 in
+  Buffer.add_string buf "{ ";
+  Hashtbl.iter (fun key value ->
+    Buffer.add_string buf (Printf.sprintf "%s: %s; " key (string_of_value value))
+  ) obj.fields;
+  Buffer.add_string buf " }";
+  Buffer.contents buf
+
+  let rec string_of_value v = match v with
+  | VInt n -> Printf.sprintf "VInt(%d)" n
+  | VBool b -> Printf.sprintf "VBool(%b)" b
+  | VObj obj -> string_of_obj obj
+  | Null -> "Null"
+
+and string_of_obj o =
+  let fields_str = Hashtbl.fold (fun key value acc ->
+    Printf.sprintf "%s: %s; %s" key (string_of_value value) acc
+  ) o.fields ""
+  in Printf.sprintf "{ %s }" fields_str
+
+let print_obj obj =
+  Printf.printf "Object: %s\n" (string_of_obj obj)
+
+let print_env env =
+  Printf.printf "Environment:\n";
+  Hashtbl.iter (fun key value ->
+    Printf.printf "  %s -> %s\n" key (string_of_value value)
+  ) env
+ *)
 let exec_prog (p: program): unit =
   let env = Hashtbl.create 16 in
   List.iter (fun (x, _) -> Hashtbl.add env x Null) p.globals;
@@ -41,7 +118,7 @@ let exec_prog (p: program): unit =
       | Error e -> raise (Error e))
       in
       current_object := None;
-      result
+      (result,obj)
     | _ -> raise (Error "Method call on non-object type")
   
   and find_class_def cls_name classes =
@@ -82,7 +159,7 @@ let exec_prog (p: program): unit =
       | VObj o ->
         (match Hashtbl.find_opt o.fields field_name with
         | Some value -> value
-        | None -> raise (Error ("Attribute not found: " ^ field_name)))
+        | None -> raise (Error ("Attribute not found: " ^ field_name)));
       | _ -> raise (Error "Field access on non-object type"))
     
 
@@ -116,7 +193,7 @@ let exec_prog (p: program): unit =
 
       | This ->
         (match !current_object with
-        | Some obj -> obj
+        | Some obj -> obj 
         | None -> raise (Error "Use of 'this' outside of object context"))
 
       | New c -> VObj { cls = c; fields = Hashtbl.create 8 }
@@ -124,15 +201,14 @@ let exec_prog (p: program): unit =
       | NewCstr (class_name, args) ->
         let obj = eval (New class_name) in
         let evaluated_args = List.map (fun arg -> eval arg) args in
-        eval_call obj "constructor" evaluated_args
+        let result = snd (eval_call obj "constructor" evaluated_args) in
+        VObj result
 
       | MethCall (obj, method_name, args) ->
         let this = eval obj in
         let evaluated_args = List.map (fun arg -> eval arg) args in
-        let obj_val = eval_call this method_name evaluated_args in
-        (match obj_val with
-        | VObj o -> VObj o
-        | _ -> raise (Error "Method call did not return an object"))
+        let obj_val = fst (eval_call this method_name evaluated_args) in
+        obj_val
 
     in
   
@@ -145,7 +221,8 @@ let exec_prog (p: program): unit =
         | Field (obj, field_name) ->
           let this = eval obj in
           (match this with
-          | VObj o -> Hashtbl.replace o.fields field_name value
+          | VObj o -> 
+            Hashtbl.replace o.fields field_name value;
           | _ -> raise (Error "Field access on non-object type")))
       | If (e, seq1, seq2) ->
         if evalb e then exec_seq seq1  else exec_seq seq2
